56f113f4298284b364694043c070ea367f7da230
coffeeknife <coffeeknife@proton.me>
2024-12-13 08:46:43 -0600

    set up module structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+mod days;
+
+#[derive(Parser)]
+struct Cli {
+    day: u64,
+    input: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match args.day {
+        1 => days::day1::day1(args.input),
+        _ => {
+            println!("Day not recognized or implemented");
+        }
+    }
 }
