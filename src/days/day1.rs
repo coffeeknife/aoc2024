8672580063848299938cc84295701cd1804c61da
coffeeknife <coffeeknife@proton.me>
2024-12-13 09:17:48 -0600

    day 1 part 2

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,12 +1,22 @@
-use std::{fs::File, io::{self, BufRead}, path::Path};
+use std::{fs::File, io::{self, BufRead}, path::Path, collections::HashMap};
+
+use itertools::Itertools;
 
 pub fn day1(input: String) {
     let (left, right) = parse_input(input);
     let mut part1: u64 = 0;
+    let mut part2: u64 = 0;
+    let right_counts: HashMap<u64, usize> = right.clone().into_iter().counts();
     for i in 0..left.len() {
-        part1 += ((*left.get(i).unwrap() as i64) - (*right.get(i).unwrap() as i64)).abs() as u64;
+        let lval: &u64 = left.get(i).unwrap();
+        part1 += ((*lval as i64) - (*right.get(i).unwrap() as i64)).abs() as u64;
+        let rcount = right_counts.get(lval);
+        if rcount.is_some() {
+            part2 += lval * (*rcount.unwrap() as u64);
+        }
     }
     println!("Part 1 Solution: {part1}");
+    println!("Part 2 Solution: {part2}");
 }
 
 fn parse_input(input: String) -> (Vec<u64>, Vec<u64>) {
