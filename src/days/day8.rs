4253be4e04a8d5c89ad176b777fe2edfea6d8f4a
coffeeknife <coffeeknife@proton.me>
2024-12-17 10:57:28 -0600

    day 8

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -3,34 +3,87 @@ use std::{collections::HashMap, fs::File, io::{BufRead, BufReader}, path::Path};
 pub fn day8(input: String) {  // TODO rewrite by just recording inside the string like day 6
     println!("[RUNNING DAY 8]");
     let (dimensions, antennae) = parse_input(input);
-    let mut antinodes: Vec<(usize, usize)> = Vec::new();
-    for key in antennae.keys() {
-        let mut points: Vec<(usize, usize)> = antennae.get(key).unwrap().clone();
-        while points.len() > 1 {
-            let cur: (usize, usize) = points.pop().expect("Somehow there are no points?");
-            for comp in &points {
-                let (anti1, anti2) = get_antinodes(cur, *comp);
-                if anti1.0 >= 0 && (anti1.0 as usize) < dimensions.0 && anti1.1 >= 0 && (anti1.1 as usize) < dimensions.1 {
-                    let a = (anti1.0 as usize, anti1.1 as usize);
-                    if !check_contains(&antinodes, &a) { antinodes.push(a); }
+    let mut anode_map: Vec<Vec<bool>> = vec![vec![false; dimensions.0]; dimensions.1];
+
+    for (_, points) in antennae.iter() {
+        let mut rem = points.clone();
+        while let Some(cur) = rem.pop() {
+            if rem.len() == 0 { break; }
+            for check in &rem {
+                let (point0, point1) = get_antinodes(cur, *check);
+                if is_good(point0, dimensions) {
+                    anode_map[point0.1 as usize][point0.0 as usize] = true;
                 }
-                if anti2.0 >= 0 && (anti2.1 as usize) < dimensions.0 && anti2.1 >= 0 && (anti2.1 as usize) < dimensions.1 {
-                    let a = (anti2.0 as usize, anti2.1 as usize);
-                    if !check_contains(&antinodes, &a) { antinodes.push(a); }
+                if is_good(point1, dimensions) {
+                    anode_map[point1.1 as usize][point1.0 as usize] = true;
                 }
             }
         }
     }
-    println!("Part 1 Solution: {}", antinodes.len())
-}
-
-fn check_contains(antinodes: &Vec<(usize, usize)>, node: &(usize, usize)) -> bool {
-    for test in antinodes {
-        if test.0 == node.0 && test.1 == node.1 {
-            return true;
+    
+    let mut part1: u64 = 0;
+    for row in anode_map {
+        for i in row {
+            if i { part1 += 1 }
         }
     }
-    false
+    println!("Part 1 Solution: {part1}");
+
+    let mut anode_map2: Vec<Vec<bool>> = vec![vec![false; dimensions.0]; dimensions.1];
+
+    for (_, points) in antennae.iter() {
+        let mut rem = points.clone();
+        while let Some(cur) = rem.pop() {
+            if rem.len() == 0 { break; }
+            for check in &rem {
+                let mut step_x: i64 = (check.0 as i64) - (cur.0 as i64);
+                let mut step_y: i64 = (check.1 as i64) - (cur.1 as i64);
+                let div = gcd(step_x.abs() as usize, step_y.abs() as usize) as i64;
+                step_x = step_x / div; step_y = step_y / div;
+                let mut cur_pos = (cur.0 as i64, cur.1 as i64);
+                while is_good(cur_pos, dimensions) {
+                    anode_map2[cur_pos.1 as usize][cur_pos.0 as usize] = true;
+                    cur_pos = (cur_pos.0 + step_x, cur_pos.1 + step_y)
+                }
+                cur_pos = (cur.0 as i64, cur.1 as i64);
+                while is_good(cur_pos, dimensions) {
+                    anode_map2[cur_pos.1 as usize][cur_pos.0 as usize] = true;
+                    cur_pos = (cur_pos.0 - step_x, cur_pos.1 - step_y)
+                }
+            }
+        }
+    }
+
+    let mut part2: u64 = 0;
+    for row in anode_map2 {
+        for i in row {
+            if i { part2 += 1 }
+        }
+    }
+    println!("Part 2 Solution: {part2}");
+}
+
+fn is_good(point: (i64, i64), dimensions: (usize, usize)) -> bool {
+    if point.0 < 0 || point.1 < 0 || point.0 >= dimensions.0 as i64 || point.1 >= dimensions.1 as i64 {
+        false
+    } else {
+        true
+    }
+}
+
+fn gcd(mut a:usize, mut b:usize) -> usize{
+    if a==b { return a; }
+    if b > a {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    while b>0 {
+        let temp = a;
+        a = b;
+        b = temp%b;
+    }
+    return a;
 }
 
 fn get_antinodes(point1: (usize, usize), point2: (usize, usize)) -> ((i64, i64), (i64, i64)) {
