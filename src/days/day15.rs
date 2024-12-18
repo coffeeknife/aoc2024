ab9cdd0448af423955e0fabffa41546a1049d0d0
coffeeknife <coffeeknife@proton.me>
2024-12-18 17:29:07 -0600

    d15pt2

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{BufRead, BufReader}, path::Path};
+use std::{fs::File, io::{BufRead, BufReader}, path::Path/*, thread::sleep, time::Duration*/};
 
 use itertools::Itertools;
 
@@ -41,10 +41,6 @@ pub fn day15(input: String) {
         }
     }
 
-    for line in map_pt2 {
-        println!("{}", line.iter().join(""))
-    }
-
     println!("Part 1 Solution: {pt1}");
     println!("Part 2 Solution: {pt2}");
 }
@@ -53,18 +49,33 @@ fn robot_move(map_start: Vec<Vec<char>>, moves: Vec<char>) -> Vec<Vec<char>> {
     let mut map: Vec<Vec<char>> = map_start.clone();
     let mut pos: (i32, i32) = find_bot(&map);
 
-    for line in &map {
-        println!("{}", line.iter().join(""))
-    }
-
     for mv in moves {
         pos = step(&mut map, pos, mv);
-        for line in &map {
+        /*for line in &map {
             println!("{}", line.iter().join(""))
         }
+        sleep(Duration::new(1, 0));*/
     }
 
     map
+}
+
+fn can_box_move(map: &Vec<Vec<char>>, pos: (i32, i32), stepv: char) -> bool {
+    if stepv == '>' || stepv == '<' { return true; } // this function isn't relevant here
+    let box_left: (i32, i32) = if get_point(map, pos) == ']' { (pos.0 - 1, pos.1) } else { pos };
+
+    let next_left: (i32, i32) = if stepv == '^' { (box_left.0, box_left.1 - 1) } else { (box_left.0, box_left.1 + 1) };
+    let next_right: (i32, i32) = (box_left.0 + 1, if stepv == '^' {box_left.1 - 1} else {box_left.1 + 1});
+    
+    (match get_point(map, next_left) {
+        '.' => true,
+        '['|']' => can_box_move(map, next_left, stepv),
+        _ => false
+    }) && match get_point(map, next_right) {
+        '.' => true,
+        '['|']' => can_box_move(map, next_right, stepv),
+        _ => false
+    }
 }
 
 fn step (map: &mut Vec<Vec<char>>, pos: (i32, i32), stepv: char) -> (i32, i32) {
@@ -79,38 +90,72 @@ fn step (map: &mut Vec<Vec<char>>, pos: (i32, i32), stepv: char) -> (i32, i32) {
         _ => println!("Warning: unrecognized step {stepv}")
     }
 
+    if this == '.' { return next_pos }
+
     match get_point(map, next_pos) {
         '.' => {
             if this == ']' {
-                let adj_pos: (i32, i32) = (pos.0 - 1, pos.1);
-                if get_point(map, adj_pos) == '[' {
-                    map[pos.1 as usize][pos.0 as usize] = '.';
-                    if !step(map, adj_pos, stepv).eq(&adj_pos) {
-                        map[next_pos.1 as usize][next_pos.0 as usize] = this;
-                        return next_pos;
-                    } else {
-                        map[pos.1 as usize][pos.0 as usize] = this;
+                map[pos.1 as usize][pos.0 as usize] = '.';
+                if next_pos.1 != pos.1 {
+                    if get_point(map, (pos.0 - 1, pos.1)) == '[' {
+                        if !can_box_move(map, pos, stepv) {
+                            map[pos.1 as usize][pos.0 as usize] = this;
                         return pos;
+                        } else {
+                            step(map, (pos.0 - 1, pos.1), stepv);
+                        }
                     }
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    map[next_pos.1 as usize][next_pos.0 as usize] = this;
+                    return next_pos;
+                } else { 
+                    map[next_pos.1 as usize][next_pos.0 as usize] = this;
+                    return next_pos;
                 }
             } else if this == '[' {
-                let adj_pos: (i32, i32) = (pos.0 + 1, pos.1);
-                if get_point(map, adj_pos) == ']' {
-                    map[pos.1 as usize][pos.0 as usize] = '.';
-                    if !step(map, adj_pos, stepv).eq(&adj_pos) {
-                        map[next_pos.1 as usize][next_pos.0 as usize] = this;
-                        return next_pos;
-                    } else {
-                        map[pos.1 as usize][pos.0 as usize] = this;
-                        return pos;
+                map[pos.1 as usize][pos.0 as usize] = '.';
+                if next_pos.1 != pos.1 {
+                    if get_point(map, (pos.0 + 1, pos.1)) == ']' {
+                        if !can_box_move(map, pos, stepv) {
+                            map[pos.1 as usize][pos.0 as usize] = this;
+                            return pos;
+                        } else {
+                            step(map, (pos.0 + 1, pos.1), stepv);
+                        }
                     }
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    map[next_pos.1 as usize][next_pos.0 as usize] = this;
+                    return next_pos;
+                } else {
+                    map[next_pos.1 as usize][next_pos.0 as usize] = this;
+                    return next_pos;
                 }
             }
             map[next_pos.1 as usize][next_pos.0 as usize] = this;
             map[pos.1 as usize][pos.0 as usize] = '.';
             return next_pos;
         }
-        'O' | ']' | '[' => {
+        '[' => {
+            if !can_box_move(&map, next_pos, stepv) { return pos; }
+            if next_pos.1 != pos.1 {
+                step(map, next_pos, stepv); step(map, (next_pos.0 + 1, next_pos.1), stepv);
+                return step(map, pos, stepv);
+            }
+            else if !step(map, next_pos, stepv).eq(&next_pos) {
+                return step(map, pos, stepv); // only recursive call if there is motion
+            }
+        }
+        ']' => {
+            if !can_box_move(&map, next_pos, stepv) { return pos; }
+            if next_pos.1 != pos.1 {
+                step(map, next_pos, stepv); step(map, (next_pos.0 - 1, next_pos.1), stepv);
+                return step(map, pos, stepv);
+            }
+            else if !step(map, next_pos, stepv).eq(&next_pos) {
+                return step(map, pos, stepv); // only recursive call if there is motion
+            }
+        }
+        'O' => {
             if !step(map, next_pos, stepv).eq(&next_pos) {
                 return step(map, pos, stepv); // only recursive call if there is motion
             }
