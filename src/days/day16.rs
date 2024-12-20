d2d0fdce238dd4c57ca997ea1392d89af7641ed9
coffeeknife <coffeeknife@proton.me>
2024-12-20 12:09:12 -0600

    DAY 16 PART 1 LFGGGG god bless the internet for teaching me how rust priority queues work + giving me data structure ideas

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,114 +1,112 @@
-use std::{collections::HashMap, fs::File, io::{BufRead, BufReader}, path::Path};
+use std::{cmp::Ordering, collections::{BinaryHeap, HashMap, HashSet}, fs::File, io::{BufRead, BufReader}, path::Path};
 
-use itertools::Itertools;
+use strum::{EnumIter, IntoEnumIterator};
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, EnumIter)]
+enum Direction { Up, Down, Left, Right }
+impl Direction {
+    fn step(&self, pt: &Point) -> Point {
+        match self {
+            Direction::Up => Point::new(pt.x, pt.y - 1),
+            Direction::Left => Point::new(pt.x - 1, pt.y),
+            Direction::Down => Point::new(pt.x, pt.y + 1),
+            Direction::Right => Point::new(pt.x + 1, pt.y)
+        }
+    }
+
+    fn opposite(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct Step { pos: Point, dir: Direction, score: usize }
+impl Step { fn new(pos: Point, dir: Direction, score: usize) -> Self { Self { pos, dir, score } } }
+impl Ord for Step { fn cmp(&self, other: &Self) -> Ordering { other.score.cmp(&self.score) } }
+impl PartialOrd for Step { fn partial_cmp(&self, other: &Self) -> Option<Ordering> { Some(self.cmp(other)) } }
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct Point { x: usize, y: usize }
+impl Point { 
+    fn new(x: usize, y: usize ) -> Self { Self { x, y } } 
+
+    fn adjacent(&self, dir: Direction) -> Vec<(Point, Direction, usize)> {
+        Direction::iter().filter(|d: &Direction| *d != dir.opposite()).map(|d: Direction| {
+            let cost: usize = if dir != d { 1001 } else { 1 };
+            (d.step(self), d, cost)
+        }).collect()
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Map {
+    spaces: HashSet<Point>,
+    start: Point,
+    end: Point
+}
+
+impl Map {
+    fn new(in_map: Vec<Vec<char>>) -> Self {
+        let mut spaces: HashSet<Point> = HashSet::new();
+        let mut start: Point = Point { x: 0, y: 0 };
+        let mut end: Point = Point { x: 0, y: 0 };
+
+        for y in 0..in_map.len() {
+            for x in 0..in_map[0].len() {
+                if in_map[y][x] == 'S' {
+                    start = Point { x, y };
+                } else if in_map[y][x] == 'E' {
+                    end = Point { x, y };
+                }
+                
+                if in_map[y][x] != '#' {
+                    spaces.insert(Point { x, y });
+                }
+            }
+        }
+
+        Self { spaces, start, end }
+    }
+
+    fn adjacent(&self, pt: Point, dir: Direction) -> Vec<(Point, Direction, usize)> {
+        pt.adjacent(dir).into_iter().filter(|(p, _, _)| self.spaces.contains(p)).collect()
+    }
+
+    fn shortest_path(&self) -> usize {
+        let mut shortest: usize = usize::MAX;
+        let mut cache: HashMap<(Point, Direction), usize> = HashMap::new();
+
+        // thank you internet for teaching me how to do binary searches
+        let mut queue: BinaryHeap<Step> = BinaryHeap::new();
+        queue.push(Step::new(self.start, Direction::Right, 0));
+
+        while let Some(Step { pos, dir, score }) = queue.pop() {
+            if let Some(&prev_score) = cache.get(&(pos, dir)) {
+                if score > prev_score { continue }
+            } else { cache.insert((pos, dir), score); }
+
+            if pos == self.end && score < shortest { 
+                shortest = score; 
+            }
+            
+            for (adj, dir_new, move_cost) in self.adjacent(pos, dir) {
+                queue.push(Step::new(adj, dir_new, score + move_cost));
+            }
+        }
+
+        shortest
+    }
+}
 
 pub fn day16(input: String) {
-    let start_map: Vec<Vec<char>> = parse_input(input);
-
-    let mut start: (usize, usize) = (0,0);
-    let mut goal: (usize, usize) = (0,0);
-
-    for y in 0..start_map.len() {
-        for x in 0..start_map[0].len() {
-            if start_map[y][x] == 'S' { start = (x, y) }
-            else if start_map[y][x] == 'E' { goal = (x, y) }
-        }
-    }
-
-    let mut cached_scores: HashMap<((usize, usize), char), usize> = HashMap::new();
-    
-    println!("Part 1 Solution: {}", lowest_score(&start, &goal, &mut start_map.clone(), '>', &mut cached_scores));
+    let in_map: Map = Map::new(parse_input(input));
+    println!("Part 1: {}", in_map.shortest_path());
 }
-
-// recursive pathfinding function
-fn lowest_score(pos: &(usize, usize), goal: &(usize, usize), test_map: &mut Vec<Vec<char>>, dir: char, cache: &mut HashMap<((usize, usize), char), usize>) -> usize {
-    if pos.eq(goal) { 
-        return 0 
-    }
-
-    test_map[pos.1][pos.0] = 'X';
-
-    _print_map(test_map);
-
-    let mut test_map_2: Vec<Vec<char>> = test_map.clone();
-
-    let mut scores: Vec<usize> = vec![usize::MAX];
-
-    // test going up
-    if get_point(&test_map, (pos.0 as i32, pos.1 as i32 - 1)) == '.' {
-        if cache.keys().contains(&((pos.0, pos.1 - 1), '^')) {scores.push(*cache.get(&((pos.0, pos.1 - 1), '^')).unwrap());}
-        else { 
-            let score: usize = safe_add(get_motion_cost(dir, '^'), lowest_score(&(pos.0, pos.1 - 1), goal, &mut test_map_2, '^', cache));
-            cache.insert(((pos.0, pos.1 - 1), '^'), score);
-            scores.push(score); 
-        }
-    }
-
-    // test going left
-    if get_point(&test_map, (pos.0 as i32 - 1, pos.1 as i32)) == '.' {
-        if cache.keys().contains(&((pos.0 - 1, pos.1), '<')) {scores.push(*cache.get(&((pos.0 - 1, pos.1), '<')).unwrap())}
-        else { 
-            let score: usize = safe_add(get_motion_cost(dir, '<'), lowest_score(&(pos.0 - 1, pos.1), goal, &mut test_map_2, '<', cache));
-            cache.insert(((pos.0 - 1, pos.1), '<'), score);
-            scores.push(score) 
-        }
-    }
-
-    // test going down
-    if get_point(&test_map, (pos.0 as i32, pos.1 as i32 + 1)) == '.' {
-        if cache.keys().contains(&((pos.0, pos.1 + 1), 'v')) {scores.push(*cache.get(&((pos.0, pos.1 + 1), 'v')).unwrap())}
-        else { 
-            let score: usize = safe_add(get_motion_cost(dir, 'v'), lowest_score(&(pos.0, pos.1 + 1), goal, &mut test_map_2, 'v', cache));
-            cache.insert(((pos.0, pos.1 + 1), 'v'), score);
-            scores.push(score); 
-        }
-    }
-
-    // test going right
-    if get_point(&test_map, (pos.0 as i32 + 1, pos.1 as i32)) == '.' {
-        if cache.keys().contains(&((pos.0 + 1, pos.1), '>')) {scores.push(*cache.get(&((pos.0 + 1, pos.1), '>')).unwrap())}
-        else { 
-            let score: usize = safe_add(get_motion_cost(dir, '>'), lowest_score(&(pos.0 + 1, pos.1), goal, &mut test_map_2, '>', cache));
-            cache.insert(((pos.0 + 1, pos.1), '>'), score);
-            scores.push(score);
-        }
-    }
-
-    let score: usize = *scores.iter().min().unwrap();
-    score
-}
-
-// debug function
-fn _print_map(map: &Vec<Vec<char>>) {
-    for line in map {
-        for entry in line {
-            print!("{entry}")
-        }
-        print!{"\n"};
-    }
-}
-
-// add without overflowing
-fn safe_add(a:usize, b:usize) -> usize {
-    if usize::MAX - a < b || usize::MAX - b < a { usize::MAX }
-    else { a + b }
-}
-
-fn get_motion_cost(a: char, b: char) -> usize {
-    if a == b || b == '*' { 1 }
-    else if a == '^' && b == 'v' || a == 'v' && b == '^' || a == '<' && b == '>' || a == '>' && b == '<' { 2001 }
-    else { 1001 }
-}
-
-// safely get point or bogus value if out of bounds
-fn get_point(map: &Vec<Vec<char>>, pt: (i32, i32)) -> char {
-    if pt.0 < 0 || pt.0 >= map[0].len() as i32 || pt.1 < 0 || pt.1 >= map.len() as i32 { '#' }
-    else {
-        let res =  map[pt.1 as usize][pt.0 as usize];
-        if res == 'E' { '.' } else { res }
-    }
-}
-
 
 fn parse_input(input: String) -> Vec<Vec<char>> {
     let file: File = File::open(Path::new(&input)).expect("Err opening file");
